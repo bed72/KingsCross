cced7a8f5ab86639846325a1f9dd20b3692095ae
bed72 <gjramos1000@gmail.com>
2023-10-09 14:25:43 -0300

    :fire: Remove GraphQL

--- src/main/kotlin/app/external/repositories/LocalUserRepository.kt
+++ src/main/kotlin/app/external/repositories/LocalUserRepository.kt
@@ -2,14 +2,12 @@ package app.external.repositories
 
 import app.external.clients.DatabaseClient
 
+import app.domain.entities.User
 import app.domain.entities.types.CreateUserType
 
-import app.data.adapters.UserAdapter
 import app.data.datasources.LocalUserDatasource
-import app.domain.entities.User
 
 class LocalUserRepository(
-    private val adapter: UserAdapter,
     private val client: DatabaseClient
 ) : LocalUserDatasource {
 //    override suspend fun create(parameter: UserModel): CreateUserType =
